a4b8b46ed35062c6cdf08789b388d177583bd7ff
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-10-08 15:56:07 +0300

    App Component

--- NEO/src/App.tsx
+++ NEO/src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import { MainView } from "./views/MainView";
+import "./index.css"; // Import Tailwind CSS styles
+
+const App: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <MainView />
+    </div>
+  );
+};
 
 export default App;
