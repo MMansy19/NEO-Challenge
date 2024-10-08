d406c587952b567819167636aae9cf180611dd58
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-10-08 15:55:15 +0300

    Filter Dropdown Component

---
+++ NEO/src/components/FilterDropdown.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+
+interface FilterDropdownProps {
+  options: string[];
+  selected: string;
+  onChange: (value: string) => void;
+}
+
+export const FilterDropdown: React.FC<FilterDropdownProps> = ({
+  options,
+  selected,
+  onChange,
+}) => {
+  return (
+    <FormControl fullWidth>
+      <InputLabel>Orbital Body</InputLabel>
+      <Select value={selected} onChange={(e) => onChange(e.target.value)}>
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
