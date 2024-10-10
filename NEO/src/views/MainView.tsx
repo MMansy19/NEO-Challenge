c2582364e1b544e7bf7a352a41c1cd6ec70a35e5
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-10-11 02:12:39 +0300

    ok

--- NEO/src/views/MainView.tsx
+++ NEO/src/views/MainView.tsx
@@ -4,8 +4,12 @@ import { DataTable } from "../components/DataTable";
 import { useFilteredNEOData } from "../hooks/useFilteredNEOData";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import { ViewSwitcher } from "../components/ViewSwitcher.tsx";
+<<<<<<< HEAD
 import { Header } from "../components/Header";
 import { SearchComp } from "../components/SearchComp";
+=======
+import { SearchBar } from "../components/SearchBar";
+>>>>>>> parent of 5987418 (enhance UI)
 
 export const MainView: React.FC = () => {
   const {
@@ -30,6 +34,7 @@ export const MainView: React.FC = () => {
     );
 
   return (
+<<<<<<< HEAD
     <div className="max-w-5xl flex flex-col gap-2 mx-auto p-8 bg-gradient-to-r from-blue-50 to-blue-100 shadow-lg rounded-xl ">
       <Header />
       <SearchComp
@@ -38,8 +43,22 @@ export const MainView: React.FC = () => {
         orbitalBodies={orbitalBodies}
         selectedOrbitalBody={selectedOrbitalBody}
         setSelectedOrbitalBody={setSelectedOrbitalBody}
+=======
+    <div className="max-w-5xl flex flex-col gap-2 mx-auto p-8 bg-gradient-to-r from-blue-50 to-blue-100 shadow-lg rounded-xl">
+      <h1 className="text-4xl font-bold text-center mb-6 text-gray-800">
+        NEO Data Visualization
+      </h1>
+
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <FilterDropdown
+        options={orbitalBodies}
+        selected={selectedOrbitalBody}
+        onChange={setSelectedOrbitalBody}
+>>>>>>> parent of 5987418 (enhance UI)
       />
+
       <ViewSwitcher view={view} setView={setView} filteredData={filteredData} />
+
       {view === "chart" ? (
         <BarChart data={filteredData} />
       ) : (
